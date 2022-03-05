50d1297f218a231e0b3c8003ef8fc6707eb97214
Nik Krav <marshaque666@gmail.com>
2022-03-05 20:46:55 +0300

    Задача 2 семинар 2

---
+++ ex6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+string Value = number.ToString();
+
+Console.WriteLine($"Число {Value} Удаление второй цифры числа {Value[0]}{Value[2]}");
+
