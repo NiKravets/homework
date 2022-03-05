ad89b0670a943f0a2edb0039a5179b068435134f
Nik Krav <marshaque666@gmail.com>
2022-03-05 21:02:58 +0300

    Задача 4 семинар 2

---
+++ ex8/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите число: ");
+string strValue = Console.ReadLine();
+int Value = int.Parse(strValue);
+
+if(Value == 6 || Value == 7) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
