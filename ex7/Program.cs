fd2905eb66401206f11c9fed44d1d3234e067aae
Nik Krav <marshaque666@gmail.com>
2022-03-05 20:56:59 +0300

    Задача 3 семинар 2

---
+++ ex7/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+string strValue = Console.ReadLine();
+int length = strValue.Length;
+
+if(length > 2) Console.WriteLine(strValue[2]);
+else Console.WriteLine("Третьей цифры нет");
