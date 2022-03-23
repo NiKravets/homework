bc66acc29d95b79e77de97b1d27bf205bc04aaab
Nik Krav <marshaque666@gmail.com>
2022-03-23 20:04:53 +0300

    возводит число A в натуральную степень B

---
+++ Задача1Семинар4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+int Number(int Value, int degree)
+{
+    int work = Value;
+    for(int i = 1; i < degree; i++ )
+    {
+        work = work * Value;
+    }
+return work;
+}
+int A = Prompt("ВВедите число А => ");
+int B = Prompt("ВВедите число B => ");
+int C = Number(A, B);
+Console.WriteLine($"{A},{B} => {C}");
