1b330361083275a51caf728042cf4cf52262c2bf
NiKravets <100012710+NiKravets@users.noreply.github.com>
2022-03-24 19:45:02 +0300

    Delete ex7 directory

--- ex7/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-Console.Write("Введите число: ");
-string strValue = Console.ReadLine();
-int length = strValue.Length;
-
-if(length > 2) Console.WriteLine(strValue[2]);
-else Console.WriteLine("Третьей цифры нет");
