9c1e8f1da03e4fc5ef14aa3b0de161d50d183a25
Nik Krav <marshaque666@gmail.com>
2022-04-08 18:54:22 +0300

    двумерный массив размером m×n, заполненный случайными вещественными числами.

---
+++ 7Семинар1задача/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble() + new Random().Next(-100, 100);
+            Console.Write("{0,6:F1}", matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+int m = Prompt("Введите число m => ");
+int n = Prompt("Введите число n => ");
+double[,] matrix = new double[m, n];
+FillArray(matrix);
