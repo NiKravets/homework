fd6396fd853af05244256b1849ca5c5a0557f708
Nik Krav <marshaque666@gmail.com>
2022-04-22 21:20:50 +0300

    Проверка на простое число

---
+++ 11Семинар2задача/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Проверка на простое число:
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+int N = Prompt("Введите число =>");
+bool prost = true;
+for (int i = 2; i <= Math.Sqrt(N); i++)
+{
+    if (N % i == 0)
+    {
+        prost = false;
+        break;
+    }
+}
+if (prost) Console.WriteLine("Это простое число");
+else Console.WriteLine("Это не простое число");
